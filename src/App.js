429ff2be680e8ef07bbf7ac20574981a6bc1359a
Winston Kim <winston.kim@brandingbrand.com>
2019-10-27 17:57:46 -0400

    Complete assignment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { range } from 'lodash';
-
-import './App.css';
+import classNames from 'classnames';
 
 const BOARD_LENGTH = 5;
 
@@ -25,7 +24,16 @@ class App extends React.Component {
 
   handleSubmit = () => {
     const { xRobot, yRobot, direction, command } = this.state;
-    if (!xRobot && !yRobot && command.split(' ')[0] !== 'PLACE') {
+    this.setState({
+      error: null,
+      report: null,
+    });
+    if (!command) {
+      this.setState({ error: "Command can't be blank" });
+      return;
+    }
+    
+    if ((!xRobot && xRobot !== 0) && (!yRobot && yRobot !== 0) && command.split(' ')[0] !== 'PLACE') {
       this.setState({ error: 'First command must be PLACE'});
       return;
     } 
@@ -114,14 +122,14 @@ class App extends React.Component {
       this.setState({ report: `${xRobot},${yRobot},${direction}`})
     } else if (command.startsWith("PLACE ")) {
       const placementPosition = command.split(" ")[1].split(',');
-      const xNew = Number(placementPosition[0]);
-      const yNew = Number(placementPosition[1]);
+      const xNew = placementPosition[0];
+      const yNew = placementPosition[1];
       const directionNew = placementPosition[2];
 
       if (xNew && yNew && directionNew && xNew > -1 && xNew < 5 && yNew > -1 && yNew < 5 && ['NORTH', 'EAST', 'SOUTH', 'WEST'].indexOf(directionNew) > -1) {
         this.setState({
-          xRobot: xNew,
-          yRobot: yNew,
+          xRobot: Number(xNew),
+          yRobot: Number(yNew),
           direction: directionNew,
         });
         return;
@@ -137,20 +145,74 @@ class App extends React.Component {
       <div className="App">
         <div className="App__board">
           {range(BOARD_LENGTH).map(y => (
-            <div key={`column-${y}`}>
+            <div key={`column-${y}`} className="App__column">
               {range(BOARD_LENGTH).map(x => (
-                <span key={`position-${x}-${y}`}>{x === xRobot && y === yRobot ? direction[0] : 0}</span>
+                <span key={`position-${x}-${y}`} className="App__cell">
+                  {x === xRobot && y === yRobot && <span className={classNames({
+                    'App__robot--north': direction === 'NORTH',
+                    'App__robot--south': direction === 'SOUTH',
+                    'App__robot--east': direction === 'EAST',
+                    'App__robot--west': direction === 'WEST',
+                  })}>→</span>}
+                </span>
               ))}
             </div>
           ))}
         </div>
         <input type="text" onChange={this.handleChange} />
         <button onClick={this.handleSubmit}>Submit</button>
-        {error && <div style={{ color: 'red' }}>{error}</div>}
+        {error && <div className="App__error">{error}</div>}
         {report && (<div className="App__report">
-          <h2>Report</h2>
           <div>{report}</div>
         </div>)}
+        <style jsx>
+          {`
+            .App {
+              text-align: center;
+              font-family: Arial;
+            }
+
+            .App__cell {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              height: 50px;
+              width: 50px;
+              background: salmon;
+              border: solid 0.5px white;
+              font-size: 20px;
+              font-weight: bold;
+            }
+
+            .App__column {
+              display: flex;
+              justify-content: center;
+            }
+
+            .App__error {
+              color: red;
+              margin-top: 12px;
+            }
+
+            .App__board {
+              display: flex;
+              flex-direction: column-reverse;
+              margin-bottom: 24px;
+            }
+
+            .App__robot--north {
+              transform: rotate(-90deg);
+            }
+
+            .App__robot--south {
+              transform: rotate(90deg);
+            }
+            
+            .App__robot--west {
+              transform: rotate(180deg);
+            }
+          `}
+        </style>
       </div>
     );
   } 
